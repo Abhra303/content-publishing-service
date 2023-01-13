95ffbb6ab430b5106c0102f9d48f96f4d3cd6cc6
Abhra303 <chakrabortyabhradeep79@gmail.com>
2023-01-13 22:36:36 +0530

    create a schema design for book model

--- content-service/book/models.py
+++ content-service/book/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+	title = models.CharField(max_length=50, null=False)
+	author = models.PositiveIntegerField("author_id", editable=False)
+	description = models.CharField(max_length=300, blank=True)
+	story = models.TextField()
+	published_date = models.DateTimeField(auto_now_add=True)
+	updated_on = models.DateTimeField(auto_now=True)
+
+	def __str__(self) -> str:
+		return "Book: " + self.title
