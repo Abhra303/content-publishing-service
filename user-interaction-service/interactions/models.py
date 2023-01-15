90a1934282c2676153bf18e552facf5e2ac14076
Abhra303 <chakrabortyabhradeep79@gmail.com>
2023-01-15 23:24:12 +0530

    user-interaction: write db model for Interaction schema

--- user-interaction-service/interactions/models.py
+++ user-interaction-service/interactions/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Interaction(models.Model):
+	READ_LIKE_CHOICES = (('R', 'Read'), ('L', 'Like'))
+	user_id = models.PositiveIntegerField()
+	content_id = models.PositiveIntegerField()
+	type = models.CharField(max_length=2, choices=READ_LIKE_CHOICES)
+	timestamp = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self) -> str:
+		return "user_id %d interaction"% (self.user_id)
