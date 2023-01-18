bced933454e874b151f26bc2a7dd32e8132d19a6
Abhra303 <chakrabortyabhradeep79@gmail.com>
2023-01-18 16:55:09 +0530

    dockerize all services

--- user-interaction-service/user_interaction_service/urls.py
+++ user-interaction-service/user_interaction_service/urls.py
@@ -16,5 +16,5 @@ Including another URLconf
 from django.urls import path, include
 
 urlpatterns = [
-    path('interactions/', include('interactions.urls')),
+    path('', include('interactions.urls')),
 ]
